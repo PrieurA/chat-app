5e954ddebf8b1a48612c6d5580e124a85c58578c
PrieurA <alexandre-prieur_student2019@wilder.school>
2020-05-27 18:26:56 +0200

    first result

--- chat-app/src/components/Contact.js
+++ chat-app/src/components/Contact.js
@@ -1,16 +1,17 @@
-https://randomuser.me/api/portraits/women/55.jpg
-Deann Harper
+const name = 'Alexandre';
+const isLoggedIn = true;
+const image = "https://avatars1.githubusercontent.com/u/47487590?s=460&u=6425b74b23778dcf23383383f1b38840de263285&v=4";
 
 const element = (
     <div className="Contact">
         <img src={image} className="avatar"></img>
         <div>
             <div className="name">{name}</div>
-            <div className="status">
+            <div className="status-text">
                 {
                     (isLoggedIn)
-                        ? <div>Online</div>
-                        : <div>Offline</div>
+                        ? <div className="status-online">Online</div>
+                        : <div className="status-offline">Offline</div>
                 }
             </div>
         </div>
