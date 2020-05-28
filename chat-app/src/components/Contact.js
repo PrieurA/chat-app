b86efd4ae318c79b5155c1608d9b48bc0f0104fc
PrieurA <alexandre-prieur_student2019@wilder.school>
2020-05-28 02:32:26 +0200

    result

--- chat-app/src/components/Contact.js
+++ chat-app/src/components/Contact.js
@@ -2,22 +2,34 @@ import React from "react";
 import PropTypes from 'prop-types';
 import './Contact.css';
 
-function Contact(props) {
-    return (
-        <div className="Contact">
-            <img src={props.avatar} alt={props.name} className="avatar"></img>
-            <div>
-                <div className="name">{props.name}</div>
-                <div className="status">
-                    {
-                        (props.online)
-                            ? <div className="status-text"><span className="status-online"></span>Online</div>
-                            : <div className="status-text"><span className="status-offline"></span>Offline</div>
-                    }
+class Contact extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { online: props.online }
+    }
+    render() {
+        return (
+            <div className="Contact">
+                <img src={this.props.avatar} alt={this.props.name} className="avatar"></img>
+                <div>
+                    <div className="name">{this.props.name}</div>
+                    <div className="status">
+                        <div
+                            onClick={event => {
+                                const newOnline = !this.state.online;
+                                this.setState({ online: newOnline });
+                            }}>
+                            {
+                                (this.state.online)
+                                    ? <div className="status-text"><span className="status-online"></span>Online</div>
+                                    : <div className="status-text"><span className="status-offline"></span>Offline</div>
+                            }
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    );
+            </div >
+        );
+    }
 }
 
 Contact.propTypes = {
